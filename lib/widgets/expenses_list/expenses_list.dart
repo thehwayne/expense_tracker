88907272e120720f1bb1c32865bffdccc1ded122
thehwayne <thehwayne@gmail.com>
2025-03-27 12:02:02 -0400

    Updates to expense_list and expense_item (added card)

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/expenses_list/expense_item.dart';
 import 'package:flutter/material.dart';
 
 class ExpensesList extends StatelessWidget {
@@ -7,9 +8,8 @@ class ExpensesList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemBuilder: (ctx, index) =>Text(
-        expenses[index].title
-        ),
-      );
+      itemCount: expenses.length,
+      itemBuilder: (ctx, index) => ExpenseItem(expense: expenses[index]),
+    );
   }
 }
