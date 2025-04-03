e8d1ca014c69b9f03524bb8a80f57cefe2a4ecee
thehwayne <thehwayne@gmail.com>
2025-04-03 11:56:16 -0400

    Update to expense.dart

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,19 +1,32 @@
+import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
+import 'package:intl/intl.dart';
 
 const uuid = Uuid();
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work
+};
+final formatter = DateFormat.yMd();
+enum Category { food, travel, leisure, work }
 
-enum Category {food, travel, leisure, work}
-
-class Expense{
+class Expense {
   Expense({
     required this.title,
     required this.amount,
     required this.date,
     required this.category,
   }) : id = uuid.v4();
+
   final String id;
   final String title;
   final double amount;
   final DateTime date;
-  final Category category; // leisure Expense(category : 'leisure') 
+  final Category category;
+   String get formattedDate{
+    return formatter.format(date);
+  }
 }
+ 
