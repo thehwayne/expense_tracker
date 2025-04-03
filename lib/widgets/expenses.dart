e26aeffd1613880c229dd1f34836e4ca017d67a7
thehwayne <thehwayne@gmail.com>
2025-04-03 11:51:41 -0400

    Updated (and fixed) expenses.dart

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,56 +1,57 @@
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:flutter/material.dart';
-import 'package:uuid/uuid.dart';
 
-class Expenses extends StatefulWidget{
+import '../models/expense.dart';
+
+class Expenses extends StatefulWidget {
   const Expenses({super.key});
-  
+
   @override
   State<Expenses> createState() {
     return _ExpensesState();
   }
 }
 
-class _ExpensesState extends State<Expenses>{
+class _ExpensesState extends State<Expenses> {
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => Text('My Modal'),
+    );
+  }
+
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Cheeseburger',
+        amount: 11.99,
+        date: DateTime.now(),
+        category: Category.food),
+    Expense(
+        title: 'Movie Ticket',
+        amount: 18.99,
+        date: DateTime.now(),
+        category: Category.leisure),
+  ];
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Expense Tracker"),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
       body: Column(
-          final List<Expense> _registeredExpenses = [
-          Expense(
-          title: 'Cheeseburger',
-          amount: 11.99,
-          date: DateTime.now(),
-          category: Category.food
-        ),
-        Expense(
-           title: 'Movie Ticket',
-          amount: 18.99,
-          date: DateTime.now(),
-          category: Category.leisure
-        ),
-        ];
+        children: [
+          Text("Chart"),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          ),
+        ],
       ),
     );
   }
 }
-
-const uuid = Uuid();
-
-enum Category {food, travel, leisure, work}
-
-class Expense{
-  Expense({
-    required this.title,
-    required this.amount,
-    required this.date,
-    required this.category,
-  }) : id = uuid.v4();
-  final String id;
-  final String title;
-  final double amount;
-  final DateTime date;
-  final Category category; // leisure Expense(category : 'leisure') 
-}
-
-
-
