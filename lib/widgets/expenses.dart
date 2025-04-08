fde236ec3ac7690363e37c9ad9ef4892c6c35e03
thehwayne <thehwayne@gmail.com>
2025-04-08 11:46:23 -0400

    Set up new_expense.dart and updated to expenses.dart

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 import '../models/expense.dart';
@@ -16,7 +17,7 @@ class _ExpensesState extends State<Expenses> {
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
       context: context,
-      builder: (ctx) => Text('My Modal'),
+      builder: (ctx) => NewExpense(),
     );
   }
 
