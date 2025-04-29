10a559c85b5b7f5536a345d53598f6984a48107e
thehwayne <thehwayne@gmail.com>
2025-04-29 12:27:51 -0400

    Visual changes implemented in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,24 @@
+import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
-import 'package:expense_tracker/widgets/expenses.dart';
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 9, 99, 190),
+);
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Expenses()
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer),
+        cardTheme: CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+      ),
+      home: Expenses(),
     ),
   );
 }
