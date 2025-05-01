924b5af8e5a52d9fd843aba41634590c038c3308
thehwayne <thehwayne@gmail.com>
2025-05-01 12:02:59 -0400

    Fixed Expenses_list (DISMISSABLE FINALLY WORKS!!!). Also, styling.

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -3,13 +3,29 @@ import 'package:expense_tracker/widgets/expenses_list/expense_item.dart';
 import 'package:flutter/material.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({super.key, required this.expenses});
+  const ExpensesList(
+      {super.key, required this.expenses, required this.onRemoveExpense});
+  final Function onRemoveExpense;
   final List<Expense> expenses;
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (ctx, index) => ExpenseItem(expense: expenses[index]),
+      itemBuilder: (ctx, index) => Dismissible(
+        key: ValueKey(expenses[index]),
+        background: Container(
+          margin: EdgeInsets.symmetric(
+            horizontal: Theme.of(context).cardTheme.margin!.horizontal,
+          ),
+          color: Theme.of(context).colorScheme.error.withValues(
+                alpha: .75,
+              ),
+        ),
+        onDismissed: (direction) {
+          onRemoveExpense(expenses[index]);
+        },
+        child: ExpenseItem(expense: expenses[index]),
+      ),
     );
   }
 }
