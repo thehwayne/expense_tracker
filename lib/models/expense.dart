e776b8bf4a486395dc1fff042440a82b27eeefc9
thehwayne <thehwayne@gmail.com>
2025-05-23 03:53:14 -0400

    added notes for recording

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,8 +1,14 @@
 import 'package:uuid/uuid.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+
+// This is a unique ID generator for each expense
 const uuid = Uuid();
+
+// Formatter for dates
 final formatter = DateFormat.yMd();
+
+// Enum for expense categories
 enum Category {
   food, 
   travel,
@@ -10,55 +16,65 @@ enum Category {
   work
 }
 
+// Mapping each category to a representative icon for UI display
 const categoryIcons = {
   Category.food : Icons.lunch_dining,
   Category.travel : Icons.flight_takeoff,
   Category.leisure: Icons.movie,
   Category.work : Icons.work
-
 };
 
-class Expense{
+// Expense Class
+class Expense {
   Expense({
     required this.title,
     required this.amount,
     required this.date,
     required this.category
-  }) : id= uuid.v4()  ;
+  }) : id = uuid.v4(); 
 
-  final String id;
-  final String title;
-  final double amount;
-  final DateTime date;
-  final Category category;
+  final String id;           
+  final String title;        
+  final double amount;       
+  final DateTime date;       
+  final Category category;   
 
-  String get formattedDate{
+  // Getter for dates
+  String get formattedDate {
     return formatter.format(date);
   }
- 
 }
- class ExpenseBucket{
-    const ExpenseBucket({
-      required this.category,
-      required this.expenses
-    });
 
-    ExpenseBucket.forCategory(
-      List<Expense> allExpenses, this.category
-    ) : expenses = allExpenses.where((expense) =>
+
+// Class for ExpenseBucket, can group multiple expenses in one category
+class ExpenseBucket {
+  const ExpenseBucket({
+    required this.category,
+    required this.expenses
+  });
+
+
+// Named constructor
+  ExpenseBucket.forCategory(
+    List<Expense> allExpenses, this.category
+  ) : expenses = allExpenses.where((expense) =>
       expense.category == category
     ).toList();
 
-    final Category category;
-    final List<Expense> expenses;
+  final Category category;       
+  final List<Expense> expenses;  
 
-    double get totalExpenses{
-      double sum = 0;
-      for(final expense in expenses)
-      {
-        sum += expense.amount;
-      }
-      return sum;
+  // This should calculate the total cost of all expenses in the bucket
+  double get totalExpenses {
+    double sum = 0;
+    for (final expense in expenses) {
+      sum += expense.amount;
     }
-    
+    return sum;
+  }
 }
+
+//"This is a bucket."
+//"Dear god."
+//"There's more."
+//"No..."
